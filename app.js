a9d2ef3d648c537543af16c7e1647bc19ae4083f
Llorch <SkylakeBeta@gmail.com>
2016-12-04 23:39:39 -0800

    Move crts and keys to certificates directory

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var fs = require('fs');
 var https = require('https');
 var app = require('express')();
 var options = {
-   key  : fs.readFileSync('server.key'),
-   cert : fs.readFileSync('server.crt')
+   key  : fs.readFileSync('./certificates/server.key'),
+   cert : fs.readFileSync('./certificates/server.crt')
 };
 
 app.get('/', function (req, res) {
